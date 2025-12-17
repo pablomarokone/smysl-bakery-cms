819ab299bd4eb4f90ae895ba62639eba77328b41
marokone <marokone@gmail.com>
2025-12-17 21:17:52 +0300

    Add CORS config via middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,33 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'], // Для будущего Cloudinary
+          'media-src': ["'self'", 'data:', 'blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://smysl-bakery.vercel.app', // Ваш сайт на Vercel
+        'http://localhost:3000',            // Для локальной разработки
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
